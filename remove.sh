342699ff91b34e63a778975bf8212226d9349bd1
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-05-08 00:43:54 +0200

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,21 @@
+sudo apt-get remove --purge mysql* -y
+
+sudo apt-get purge mysql* -y
+
+sudo apt-get autoremove -y
+
+sudo apt-get autoclean -y
+
+sudo apt-get remove dbconfig-mysql -y
+
+sudo apt-get dist-upgrade -y
+
+sudo apt-get remove phpmyadmin -y
+
+sudo apt-get purge phpmyadmin -y
+
+sudo apt-get autoremove -y
+
+sudo apt-get update -y 
+
+apt-get upgrade -y
