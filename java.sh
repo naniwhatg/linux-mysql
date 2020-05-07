ba2a36cb38169b694b676a3e9abbb9ee7fb50e4a
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-05-08 00:43:57 +0200

    Create java.sh

---
+++ java.sh
@@ -0,0 +1,17 @@
+sudo apt-get update -y
+
+sudo apt-get install default-jre -y
+
+sudo apt-get install default-jdk -y
+
+sudo apt-get install software-properties-common -y
+
+sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" -y
+
+sudo apt-get update -y
+
+sudo apt-get upgrade -y
+
+sudo apt-get install oracle-java8-installer -y
+
+javac -version
